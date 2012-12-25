63862405ef80743aa33a7160f91870a8be36673d
Richard Hart <richhohart@gmail.com>
2012-12-25 13:06:22 -0500

    Creating Demo
    A demo app to help test and set up the rails enviroment.

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '4c3277875f309c317a05c4157ed5c81a7d8f8160e27452ead1b1e52c3e2071e381682d7c4319a3cb19168da79785f73b2e7013433126d4ddd5d4ce4dcbefedd5'
